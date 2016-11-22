a00d2d774138e7b8fe47c49cca65b7be6b0c8749
Xuejing Luo <xuejingluo@XuejingdeMacBook-Pro.local>
2016-11-22 18:02:33 +0800

    first commit(simple-build)

---
+++ index.js
@@ -0,0 +1,3 @@
+import { cube } from "lib/maths";
+
+console.log( cube( 5 ) );
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+	entry: 'index.js',
+	format: 'cjs',
+	// format: 'umd', //其他形式
+	// format: 'iife', //其他形式
+	// format: 'amd', //其他形式
+	dest: 'public/bundle.js'
+}
