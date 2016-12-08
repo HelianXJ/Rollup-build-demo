34c2d1e5fcc94cb6b040701819b2b43414ca73f9
Xuejing Luo <xuejingluo@XuejingdeMacBook-Pro.local>
2016-12-08 18:23:49 +0800

    加入require页面展示js结果

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
 	entry: 'index.js',
-	format: 'cjs',
+	// format: 'cjs',
 	// format: 'umd', //其他形式
 	// format: 'iife', //其他形式
-	// format: 'amd', //其他形式
+	format: 'amd', //其他形式   
+	plugins: [babel()],                    
 	dest: 'public/bundle.js'
 }
